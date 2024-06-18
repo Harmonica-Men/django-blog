37dd0a91616498c8063f7cd980528242cf41709b
Harmonica-Men <filip@vanelslande.com>
2024-06-18 17:30:36 +0200

    update requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [
+                 '.herokuapp.com', 
+                 '.127.0.0.1',
+                ]
 
 
 # Application definition
@@ -51,6 +54,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'cloudinary',
     'blog',
+
 ]
 
 MIDDLEWARE = [
